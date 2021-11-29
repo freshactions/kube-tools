2a0b24ae8fe5a1fdd027e3e08ce67d14a1076ab0
Namitha Sudhakaran <namitha.sudhakaran@freshworks.com>
2021-11-29 12:16:48 +0530

    [ED-1000] update kustomize to 3.7.0

--- src/install.sh
+++ src/install.sh
@@ -8,7 +8,7 @@ curl -sL https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL}/b
 -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
-KUSTOMIZE=3.5.5
+KUSTOMIZE=3.7.0
 echo "downloading kustomize ${KUSTOMIZE}"
 curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE}/kustomize_v${KUSTOMIZE}_linux_amd64.tar.gz | \
 tar xz && mv kustomize /usr/local/bin/kustomize
